1a9822be7ff9c78e29e36638e2e0f8aa2000e21e
Shadowbeetle <tamas.kadlecsik@gmail.com>
2024-01-31 23:46:59 +0100

    add install button

--- install.js
+++ install.js
@@ -1,9 +1,14 @@
+let deferredPrompt;
 window.addEventListener("beforeinstallprompt", (e) => {
   document.getElementById("beforeinstallprompt").innerHTML = "✔️";
-  e.prompt();
+  deferredPrompt = e;
 });
 
 // Hide the install button
 window.addEventListener("appinstalled", (evt) => {
   document.getElementById("appinstalled").innerHTML = "✔️";
+});
+
+document.getElementById("install").addEventListener("click", (e) => {
+  deferredPrompt.prompt();
 });
